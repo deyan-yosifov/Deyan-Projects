1ab80b745366d8e84d93b9a410656d45a64e4161
deyan-yosifov <yosifov.deyan@gmail.com>
2013-03-31 00:06:00 +0200

    Adding first test for JavaScript part 1 Exam Preparations

---
+++ JavaScript/HWs-Telerik/HW-JS1-Exam-Preparations/CS1-Trapecoid.js
@@ -0,0 +1,24 @@
+﻿String.prototype.PadLeft = function (count, symbol) {
+    var result = this;
+    var boundary = count - result.length;
+    for (var i = 0; i < boundary; i++) result = symbol + result;
+    return result;
+}
+
+String.prototype.PadRight = function (count, symbol) {
+    var result = this;
+    var boundary = count - result.length;
+    for (var i = 0; i < boundary; i++) result = result + symbol;
+    return result;
+}
+
+function Solve(args) {
+    var result = "";
+    var n = parseInt(args);
+    result += "*".PadRight(n,'*').PadLeft(2*n,'.') + "\n";
+    for (var i = 0; i < n-1; i++) {
+        result += "*".PadLeft(n - i, '.').PadRight(2 * n - 1, '.') + "*\n";
+    }
+    result += "*".PadRight(2 * n, '*');
+    return result;
+}
