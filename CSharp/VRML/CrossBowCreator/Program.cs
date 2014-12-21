f49b4700a46ec17d04e27c498b133cc94b2c0ae7
deyan-yosifov <yosifov.deyan@gmail.com>
2014-12-22 01:37:45 +0200

    still lots of job on VRML format provider

--- CSharp/VRML/CrossBowCreator/Program.cs
+++ CSharp/VRML/CrossBowCreator/Program.cs
@@ -5,6 +5,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+using Vrml.FormatProvider;
+using Vrml.Model;
 
 namespace CrossBowCreator
 {
@@ -14,15 +17,29 @@ namespace CrossBowCreator
 
         static void Main(string[] args)
         {
+            VrmlDocument document = GenerateCrossBowDocument();
 
             if (File.Exists(ExportFileName))
             {
                 File.Delete(ExportFileName);
             }
 
-            // TODO: Create file here
+            using (Stream fileStream = new FileStream(ExportFileName, FileMode.OpenOrCreate))
+            {
+                VrmlFormatProvider provider = new VrmlFormatProvider();
+                provider.Export(document, fileStream);
+            }
 
             Process.Start(ExportFileName);
+        }
+
+        private static VrmlDocument GenerateCrossBowDocument()
+        {
+            VrmlDocument document = new VrmlDocument();
+            document.Title = "Cross Bow by Deyan Yosifov";
+            document.Viewpoint = new Viewpoint() { Position = new Point3D(0, 1, 2.5) };
+
+            return document;
         }
     }
 }
--- CSharp/VRML/Vrml/FormatProvider/Exporter.cs
+++ CSharp/VRML/Vrml/FormatProvider/Exporter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
 using Vrml.Model;
 
 namespace Vrml.FormatProvider
@@ -11,6 +12,8 @@ namespace Vrml.FormatProvider
     {
         private const char Offset = '\t';
         private const char Space = ' ';
+        private const char LeftBracket = '{';
+        private const char RightBracket = '}';
         private readonly VrmlDocument document;
         private readonly StringBuilder builder;
 
@@ -40,6 +43,9 @@ namespace Vrml.FormatProvider
         {
             this.ExportDocumentStart();
             this.ExportWorldInfo();
+            this.ExportViewpoint();
+
+
 
             return this.Builder.ToString();
         }
@@ -55,10 +61,47 @@ namespace Vrml.FormatProvider
         {
             if (document.Title != null)
             {
-                this.Builder.AppendFormat(@"WorldInfo {
-	title ""{0}""
-}", this.Document.Title);
+                this.Builder.AppendFormat(@"WorldInfo {0}
+	title ""{1}""
+{2}", LeftBracket, this.Document.Title, RightBracket);
             }
+        }
+
+        private void ExportViewpoint()
+        {
+            if (document.Viewpoint != null)
+            {
+                this.Builder.AppendLine("Viewpoint {");
+
+                if (document.Viewpoint.Position.HasValue)
+                {
+                    this.Builder.AppendFormat("{0}position ", Offset);
+                    this.ExportPosition(document.Viewpoint.Position.Value);
+                    this.Builder.AppendLine();
+                }
+
+                if (document.Viewpoint.Orientation != null)
+                {
+                    this.Builder.AppendFormat("{0}orientation ", Offset);
+                    this.ExportOrientation(document.Viewpoint.Orientation);
+                    this.Builder.AppendLine();
+                }
+
+                this.Builder.AppendLine(@"
+	description ""Entry view""
+}");
+                this.Builder.AppendLine();
+            }
+        }
+
+        private void ExportPosition(Point3D point)
+        {
+            this.Builder.AppendFormat("{0} {1} {2}", point.X, point.Y, point.Z);
+        }
+
+        private void ExportOrientation(Orientation orientation)
+        {
+            this.Builder.AppendFormat("{0} {1} {2} {3}", orientation.Vector.X, orientation.Vector.Y, orientation.Vector.Z, orientation.Angle);
         }
     }
 }
