334e57c310258fd3b261a458904d48c4b71a3639
deyan-yosifov <yosifov.deyan@gmail.com>
2014-12-31 14:17:25 +0200

    Started work on incomes expenses report generator

---
+++ JavaScript/Google/Sheets/Incomes expenses report generator.js
@@ -0,0 +1,44 @@
+/**
+ * Retrieves all the rows in the active spreadsheet that contain data and logs the
+ * values for each row.
+ * For more information on using the Spreadsheet API, see
+ * https://developers.google.com/apps-script/service_spreadsheet
+ */
+function readRows() {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var rows = sheet.getDataRange();
+  var numRows = rows.getNumRows();
+  var values = rows.getValues();
+
+  var text = "";
+  
+  for (var i = 0; i <= numRows - 1; i++) {
+    var row = values[i];
+    text += row + " row data\n";
+    //Logger.log(row);
+  }
+  
+  var ui = SpreadsheetApp.getUi();
+  
+  var result = ui.alert(
+     'See rows data!',
+      text,
+      ui.ButtonSet.YES_NO);
+};
+
+/**
+ * Adds a custom menu to the active spreadsheet, containing a single menu item
+ * for invoking the readRows() function specified above.
+ * The onOpen() function, when defined, is automatically invoked whenever the
+ * spreadsheet is opened.
+ * For more information on using the Spreadsheet API, see
+ * https://developers.google.com/apps-script/service_spreadsheet
+ */
+function onOpen() {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var entries = [{
+    name : "Read Data",
+    functionName : "readRows"
+  }];
+  spreadsheet.addMenu("Incomes expenses generator", entries);
+};
