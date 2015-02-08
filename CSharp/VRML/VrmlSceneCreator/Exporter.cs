565f7c28a56e460d0e85cd358d7237ca0506eacc
deyan-yosifov <yosifov.deyan@gmail.com>
2015-02-08 11:28:32 +0200

    VRML Scene creator project started

---
+++ CSharp/VRML/VrmlSceneCreator/Exporter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VrmlSceneCreator
+{
+    public class Exporter
+    {
+        private readonly VrmlExportContext context;
+
+        public Exporter()
+        {
+            this.context = new VrmlExportContext();
+        }
+
+        public void Readline(string line)
+        {
+
+        }
+    }
+}
