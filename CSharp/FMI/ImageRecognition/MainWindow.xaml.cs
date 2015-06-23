df16e457e3726d6e9adabdf353169118fc1005c5
deyan-yosifov <yosifov.deyan@gmail.com>
2015-06-23 20:59:38 +0300

    Started work on ImageRecognition project

---
+++ CSharp/FMI/ImageRecognition/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace ImageRecognition
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string ImagesDatabaseFolderName = "ImagesDatabase";
+        private const string DatabaseFileName = "database.json";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.EnsureDatabaseExists();
+        }
+
+        private void EnsureDatabaseExists()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string databaseFolderPath = Path.Combine(dir, ImagesDatabaseFolderName);
+
+            if (!Directory.Exists(databaseFolderPath))
+            {
+                Directory.CreateDirectory(databaseFolderPath);
+            }
+
+            string databaseFilePath = Path.Combine(databaseFolderPath, DatabaseFileName);
+            if (!File.Exists(databaseFilePath))
+            {
+                File.Create(databaseFilePath);
+            }
+        }
+    }
+}
