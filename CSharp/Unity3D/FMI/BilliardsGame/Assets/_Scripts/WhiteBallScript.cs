c2442740c06a8682dede1c7e1f8ed48c7f2fe1b2
deyan-yosifov <yosifov.deyan@gmail.com>
2015-07-07 11:51:42 +0300

    Started Billiard Unity3D game.

---
+++ CSharp/Unity3D/FMI/BilliardsGame/Assets/_Scripts/WhiteBallScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhiteBallScript : MonoBehaviour {
+
+    public float ballSpeed;
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        this.rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        this.rigidbody.AddForce(new Vector3(moveHorizontal, 0, moveVertical) * this.ballSpeed);
+    }
+}
