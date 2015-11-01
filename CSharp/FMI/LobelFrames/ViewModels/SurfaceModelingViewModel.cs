e68a225f9dc8364b614c0ae814697277d22d4000
deyosoft <deyosoft@gmail.com>
2015-11-01 17:42:52 +0200

    Started LobelFrames project

---
+++ CSharp/FMI/LobelFrames/ViewModels/SurfaceModelingViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Deyo.Controls.Common;
+using Deyo.Controls.Controls3D;
+using System;
+
+namespace LobelFrames.ViewModels
+{
+    public class SurfaceModelingViewModel : ViewModelBase
+    {
+        private readonly Scene3D scene;
+
+        public SurfaceModelingViewModel(Scene3D scene)
+        {
+            this.scene = scene;
+        }
+    }
+}
---
+++ CSharp/FMI/LobelFrames/Views/SurfaceModelingView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using LobelFrames.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LobelFrames.Views
+{
+    /// <summary>
+    /// Interaction logic for SurfaceModelingView.xaml
+    /// </summary>
+    public partial class SurfaceModelingView : UserControl
+    {
+        private readonly SurfaceModelingViewModel viewModel;
+
+        public SurfaceModelingView()
+        {
+            InitializeComponent();
+            this.viewModel = new SurfaceModelingViewModel(this.scene);
+            this.DataContext = this.viewModel;
+        }
+    }
+}
