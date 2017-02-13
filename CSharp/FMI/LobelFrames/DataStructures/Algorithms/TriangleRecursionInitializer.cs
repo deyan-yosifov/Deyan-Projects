8ac2135ebd9db0335e415d7a1c0fc980e6064459
deyan-yosifov <yosifov.deyan@gmail.com>
2017-02-13 20:33:39 +0200

    Kind of implemented distance from triangle precedence for choosing best initial recursion position.

--- CSharp/FMI/LobelFrames/DataStructures/Algorithms/TriangleRecursionInitializer.cs
+++ CSharp/FMI/LobelFrames/DataStructures/Algorithms/TriangleRecursionInitializer.cs
@@ -3,6 +3,7 @@ using Deyo.Core.Mathematics.Algebra;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using System.Windows.Media.Media3D;
 
 namespace LobelFrames.DataStructures.Algorithms
@@ -10,90 +11,69 @@ namespace LobelFrames.DataStructures.Algorithms
     internal sealed class TriangleRecursionInitializer : IDisposable
     {
         private readonly Triangle triangle;
+        private readonly Vector3D triangleUnitNormal;
+        private readonly Point3D triangleCenter;
         private readonly OctaTetraApproximationContext context;
+        private readonly ComparableRecursionPosition?[] sidesRecursionPositions;
         private bool isDisposed;
 
         public TriangleRecursionInitializer(Triangle triangle, OctaTetraApproximationContext context)
         {
             this.triangle = triangle;
             this.context = context;
+            this.sidesRecursionPositions = new ComparableRecursionPosition?[3];            
+            Vector3D normal = Vector3D.CrossProduct(this.triangle.B.Point - this.triangle.A.Point, this.triangle.C.Point - this.triangle.A.Point);
+            normal.Normalize();
+            this.triangleUnitNormal = normal;
+            this.triangleCenter = this.triangle.A.Point + (1.0 / 3) * ((this.triangle.B.Point - this.triangle.A.Point) + (this.triangle.C.Point - this.triangle.A.Point));
+
             this.isDisposed = false;
         }
 
-        private Point3D A
-        {
-            get
-            {
-                return this.triangle.A.Point;
-            }
-        }
-
-        private Point3D B
-        {
-            get
-            {
-                return this.triangle.B.Point;
-            }
-        }
-
-        private Point3D C
-        {
-            get
-            {
-                return this.triangle.C.Point;
-            }
-        }
-
-        private UVMeshDescretePosition? AInitialRecursionPosition { get; set; }
-
-        private UVMeshDescretePosition? BInitialRecursionPosition { get; set; }
-
-        private UVMeshDescretePosition? CInitialRecursionPosition { get; set; }
-        
         public void UpdatePositionInitializations(UVMeshDescretePosition positionToCheck, Point3D barycentricCoordinates)
         {
-            Guard.ThrowExceptionIfTrue(this.isDisposed, "hasEnqueuedSteps");
+            Guard.ThrowExceptionIfTrue(this.isDisposed, "isDisposed");
 
-            if (barycentricCoordinates.X.IsLessThan(0))
+            for(int sideIndex = 0; sideIndex < 3; sideIndex++)
             {
-                this.AInitialRecursionPosition = positionToCheck;
-            }
+                ComparableRecursionPosition currentPosition = new ComparableRecursionPosition(positionToCheck, barycentricCoordinates, sideIndex);
 
-            if (barycentricCoordinates.Y.IsLessThan(0))
-            {
-                this.BInitialRecursionPosition = positionToCheck;
-            }
+                if(currentPosition.BelongsToRecursionSemiplane)
+                {
+                    ComparableRecursionPosition? previousPosition = this.sidesRecursionPositions[sideIndex];
 
-            if (barycentricCoordinates.Z.IsLessThan(0))
-            {
-                this.CInitialRecursionPosition = positionToCheck;
+                    if (previousPosition.HasValue)
+                    {
+                        if (currentPosition.CompareTo(previousPosition.Value) < 0)
+                        {
+                            this.sidesRecursionPositions[sideIndex] = currentPosition;
+                        }
+                    }
+                    else
+                    {
+                        this.sidesRecursionPositions[sideIndex] = currentPosition;
+                    }
+                }
             }
         }
 
         private IEnumerable<OctaTetraApproximationStep> CalculateRecursionNextApproximationSteps()
         {
-            OctaTetraApproximationStep aStep;
-            if(this.TryCalculateApproximationStep(this.AInitialRecursionPosition, 0, out aStep))
+            for (int sideIndex = 0; sideIndex < 3; sideIndex++)
             {
-                yield return aStep;
-            }
-
-            OctaTetraApproximationStep bStep;
-            if (this.TryCalculateApproximationStep(this.BInitialRecursionPosition, 1, out bStep))
-            {
-                yield return bStep;
-            }
-
-            OctaTetraApproximationStep cStep;
-            if (this.TryCalculateApproximationStep(this.CInitialRecursionPosition, 2, out cStep))
-            {
-                yield return cStep;
+                OctaTetraApproximationStep step;
+                if (this.TryCalculateApproximationStep(sideIndex, out step))
+                {
+                    yield return step;
+                }
             }
         }
 
-        private bool TryCalculateApproximationStep(UVMeshDescretePosition? recursionPosition, int sideIndex, out OctaTetraApproximationStep step)
+        private bool TryCalculateApproximationStep(int sideIndex, out OctaTetraApproximationStep step)
         {
-            if(recursionPosition.HasValue)
+            ComparableRecursionPosition? recursionPosition = this.sidesRecursionPositions[sideIndex];
+
+            if (recursionPosition.HasValue)
             {
                 Triangle[] bundle = this.CreateNonExistingNeigbouringTriangles(sideIndex).ToArray();
 
@@ -101,7 +81,7 @@ namespace LobelFrames.DataStructures.Algorithms
                 {
                     step = new OctaTetraApproximationStep()
                     {
-                        InitialRecursionPosition = recursionPosition.Value,
+                        InitialRecursionPosition = recursionPosition.Value.MeshPosition,
                         TrianglesBundle = bundle
                     };
 
@@ -118,11 +98,8 @@ namespace LobelFrames.DataStructures.Algorithms
             Vertex opositeVertex = this.triangle.GetVertex(sideIndex);
             Vertex edgeStart = this.triangle.GetVertex((sideIndex + 1) % 3);
             Vertex edgeEnd = this.triangle.GetVertex((sideIndex + 2) % 3);
-            Vector3D triangleUnitNormal = Vector3D.CrossProduct(this.B - this.A, this.C - this.A);
-            triangleUnitNormal.Normalize();
 
-            Point3D triangleCenter = opositeVertex.Point + (1.0 / 3) * ((edgeStart.Point - opositeVertex.Point) + (edgeEnd.Point - opositeVertex.Point));
-            Point3D tetrahedronTop = triangleCenter + this.context.TetrahedronHeight * triangleUnitNormal;
+            Point3D tetrahedronTop = this.triangleCenter + this.context.TetrahedronHeight * this.triangleUnitNormal;
             Point3D edgeCenter = edgeStart.Point + 0.5 * (edgeEnd.Point - edgeStart.Point);
             Point3D octahedronPoint = edgeCenter + (edgeCenter - opositeVertex.Point);
             Point3D oppositeTetrahedronTop = edgeCenter + (edgeCenter - tetrahedronTop);
@@ -155,6 +132,105 @@ namespace LobelFrames.DataStructures.Algorithms
             foreach (OctaTetraApproximationStep step in this.CalculateRecursionNextApproximationSteps())
             {
                 this.context.RecursionQueue.Enqueue(step);
+            }
+        }
+
+        private struct ComparableRecursionPosition : IComparable<ComparableRecursionPosition>
+        {
+            private readonly UVMeshDescretePosition meshPosition;
+            private readonly double distanceFromSide;
+            private readonly double distanceFromTriangle;
+            private readonly bool belongsToRecursionSemiplane;
+            private static Point relativeTriangleA = new Point(1, 0);
+            private static Point relativeTriangleB = new Point(-1, 0);
+            private static Point relativeTriangleC = new Point(0, Math.Sqrt(3));
+            private static double third = 1.0 / 3.0;
+
+            public ComparableRecursionPosition(UVMeshDescretePosition meshPosition, Point3D barycentricCoordinates, int sideIndex)
+            {
+                this.meshPosition = meshPosition;
+
+                double sideCoordinate, firstNeighbourCoordinate, secondNeighbourCoordinate;
+                GetCoordinatesRelativeToSide(barycentricCoordinates, sideIndex, out sideCoordinate, out firstNeighbourCoordinate, out secondNeighbourCoordinate);
+                this.belongsToRecursionSemiplane = sideCoordinate.IsLessThan(0);
+                this.distanceFromSide = -sideCoordinate;
+
+                if (this.belongsToRecursionSemiplane)
+                {
+                    bool isInsideTriangle = distanceFromSide <= 1
+                        && firstNeighbourCoordinate >= 0 && firstNeighbourCoordinate <= 1
+                        && secondNeighbourCoordinate >= 0 && secondNeighbourCoordinate <= 1;
+
+                    if (isInsideTriangle)
+                    {
+                        this.distanceFromTriangle = 0;
+                    }
+                    else
+                    {
+                        double x = sideCoordinate + third;
+                        double y = firstNeighbourCoordinate - third;
+                        double z = secondNeighbourCoordinate - third;
+                        double xSum = (x * relativeTriangleA.X + y * relativeTriangleB.X + z * relativeTriangleC.X);
+                        double ySum = (x * relativeTriangleA.Y + y * relativeTriangleB.Y + z * relativeTriangleC.Y);
+                        this.distanceFromTriangle = xSum * xSum + ySum * ySum;
+                    }
+                }
+                else
+                {
+                    this.distanceFromTriangle = double.MaxValue;
+                }
+            }
+
+            public UVMeshDescretePosition MeshPosition
+            {
+                get
+                {
+                    return this.meshPosition;
+                }
+            }
+
+            public bool BelongsToRecursionSemiplane
+            {
+                get
+                {
+                    return this.belongsToRecursionSemiplane;
+                }
+            }
+
+            public int CompareTo(ComparableRecursionPosition other)
+            {
+                if(this.distanceFromTriangle == 0 && other.distanceFromTriangle == 0)
+                {
+                    return this.distanceFromSide.CompareTo(other.distanceFromSide);
+                }
+                else
+                {
+                    return this.distanceFromTriangle.CompareTo(other.distanceFromTriangle);
+                }
+            }
+
+            private static void GetCoordinatesRelativeToSide(Point3D barycentricCoordinates, int sideIndex, out double sideCoordinate, out double firstNeighbour, out double secondNeighbour)
+            {
+                switch (sideIndex)
+                {
+                    case 0:
+                        sideCoordinate = barycentricCoordinates.X;
+                        firstNeighbour = barycentricCoordinates.Y;
+                        secondNeighbour = barycentricCoordinates.Z;
+                        break;
+                    case 1:
+                        sideCoordinate = barycentricCoordinates.Y;
+                        firstNeighbour = barycentricCoordinates.X;
+                        secondNeighbour = barycentricCoordinates.Z;
+                        break;
+                    case 2:
+                        sideCoordinate = barycentricCoordinates.Z;
+                        firstNeighbour = barycentricCoordinates.X;
+                        secondNeighbour = barycentricCoordinates.Y;
+                        break;
+                    default:
+                        throw new ArgumentException(string.Format("sideIndex cannot be {0}", sideIndex));
+                }
             }
         }
     }
